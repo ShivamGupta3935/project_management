0cc1c3f23d13f00b45861e90749e512fff0f8eba
Shivam Gupta <guptashivam3935@gmail.com>
2026-09-07 20:26:14 +0530

    adding usermodel and hooks

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,7 +1,70 @@
-import mongoose, {Schema} from "mongoose"
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
 
-const userSchema = new Schema({
-    avatar: {
-        
-    }
+const userSchema = new Schema(
+   {
+      avatar: {
+         type: {
+            url: String,
+            localPath: String,
+         },
+         default: {
+            url: "",
+            localPath: "",
+         },
+      },
+      username: {
+         type: String,
+         required: true,
+         trim: true,
+         lowercase: true,
+         unique: true,
+         index: true,
+      },
+      email: {
+         type: String,
+         required: true,
+         unique: true,
+         lowercase: true,
+         trim: true,
+      },
+      fullname: {
+         type: String,
+         required: true,
+      },
+      refreshToken: {
+         type: String,
+      },
+      password: {
+         type: String,
+         required: [true, "password is required"],
+      },
+      isEmailVerified: {
+         type: Boolean,
+         default: false,
+      },
+      forgotPasswordToken: {
+         type: String,
+      },
+      forgotPasswordExpiry: {
+         type: Date,
+      },
+      emailVerificationToken: {
+         type: String,
+      },
+      emailVerificationExpiry: {
+         type: Date,
+      },
+   },
+   {
+      timestamps: true,
+   },
+);
+
+userSchema.pre("save", async function (next) {
+   if (!this.isModified("password")) return next()
+   this.password = bcrypt.hash(this.password, 10)
+   next()
 })
+
+export const User = mongoose.model("User", userSchema)
