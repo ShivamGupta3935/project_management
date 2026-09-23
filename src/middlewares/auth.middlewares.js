0a3ba89508ef7a774d486b9189590fd6fe747db2
Shivam Gupta <guptashivam3935@gmail.com>
2026-09-23 22:44:29 +0530

    adding auth middleware

---
+++ src/middlewares/auth.middlewares.js
@@ -0,0 +1,30 @@
+import asyncHandler from "../utils/async-handler.js";
+import { User } from "../models/user.models.js";
+import ApiError from "../utils/api-error.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJWT = asyncHandler(async (req, _, next) => {
+   try {
+      const token =
+         req.cookies?.accessToken ||
+         req.header("Authorization")?.replace("Bearer ", "");
+
+      if (!token) {
+         throw new ApiError(401, "Unauthorized access token");
+      }
+
+      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+      if (!decodedToken) {
+         throw new ApiError(401, "Invalid token");
+      }
+
+      const user = await User.findById(decodedToken._id).select("-password -refreshToken -emailVerificationToken -emailTokenExpiry");
+      if (!user) {
+         throw new ApiError(401, "Invalid access token");
+      }
+
+      req.user = user;
+   } catch (error) {
+      throw new ApiError(401, "Unauthorized access token");
+   }
+});
